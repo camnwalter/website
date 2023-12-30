8663a5fc7230e3743655b32274f02e475051caad
Matthew Olsson <matthewcolsson@gmail.com>
2023-12-30 13:22:07 -0800

    Drastically simplify the SearchBar
    Now you can either search module names, descriptions, tags, summaries,
    or owners, but not a combination of them. It's way less janky, both
    from a code and user perspective.

--- app/SearchBar.tsx
+++ app/SearchBar.tsx
@@ -1,110 +1,22 @@
 "use client";
 
-import { QuestionMark } from "@mui/icons-material";
-import { Box, Chip, ChipDelete, IconButton, Input, Stack, Tooltip, Typography } from "@mui/joy";
+import { FilterList, QuestionMark } from "@mui/icons-material";
+import { Autocomplete, Box, Chip, ChipDelete, Dropdown, IconButton, Input, Menu, MenuButton, MenuItem, Stack, Tooltip, Typography } from "@mui/joy";
 import type { SxProps, Theme } from "@mui/material/styles";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { switchMode } from "utils/layout";
 
-const helpText = (
-  <Typography level="body-sm" sx={{ color: "#ddd" }}>
-    Searches module names, descriptions, and authors. Use <br />
-    <code style={{ backgroundColor: "#555" }}>name:&lt;value&gt;</code> to filter by name,{" "}
-    <code style={{ backgroundColor: "#555" }}>author:&lt;value&gt;</code>
-    <br />
-    to filter for author, and <code style={{ backgroundColor: "#555" }}>tag:&lt;value&gt;</code> to
-    filter by tags.
-  </Typography>
-);
-
 interface Props {
   sx?: SxProps<Theme>;
 }
 
-interface Term {
-  name: string;
-  value: string;
-}
-
-interface TermWithNode extends Term {
-  node: React.ReactNode;
-}
-
-const termOptions = ["name", "owner", "description", "tag"].map(tag => {
-  return [tag, new RegExp(`(?:^| )(?<tag>${tag}:\\w+) `)] as const;
-});
-
-function splitSearchIntoTerms(input: string): { terms: Term[]; remaining: string } {
-  const terms = [];
-  let remaining = input;
-
-  for (const option of termOptions) {
-    const match = option[1].exec(remaining);
-    if (!match) continue;
-
-    const [name, value] = match[1].split(":");
-    terms.push({ name, value });
-
-    const firstIndex = remaining.indexOf(match[0]);
-    const lastIndex = match.index + match[0].length;
-    remaining = remaining.substring(0, firstIndex) + remaining.substring(lastIndex);
-  }
-
-  return { terms, remaining };
-}
-
 export default function SearchBar({ sx = [] }: Props) {
-  const makeTermNode = (term: Term): TermWithNode => ({
-    ...term,
-    node: (
-      <Chip
-        key={term.name + term.value}
-        variant="solid"
-        sx={{ backgroundColor: theme => theme.vars.palette.neutral[500] }}
-        endDecorator={
-          <ChipDelete
-            sx={{ ml: 0, backgroundColor: theme => theme.vars.palette.neutral[600] }}
-            variant="outlined"
-            onDelete={() => handleDeleteChip(term)}
-          />
-        }
-      >
-        {term.name}:{term.value}
-      </Chip>
-    ),
-  });
-
   const router = useRouter();
   const query = useSearchParams();
 
-  const getTermNodes = (): TermWithNode[] => {
-    return Array.from(query)
-      .filter(([name]) => termOptions.find(o => o[0] === name))
-      .map(([name, value]) => ({ name, value }))
-      .map(makeTermNode);
-  };
-
-  const [value, setValue] = useState(query.get("q"));
-  const [terms, setTerms] = useState<TermWithNode[]>(getTermNodes());
-
-  useEffect(() => {
-    setValue(query.get("q"));
-    setTerms(getTermNodes());
-  }, [query]);
-
-  const handleDeleteChip = ({ name, value }: Term): void => {
-    setTerms(terms.filter(c => c.name !== name || c.value !== value));
-  };
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    const value = event.target.value;
-    const { terms: newTerms, remaining } = splitSearchIntoTerms(value);
-    if (newTerms.length) {
-      setTerms([...terms, ...newTerms.map(makeTermNode)]);
-    }
-    setValue(remaining);
-  };
+  const [searchKind, setSearchKind] = useState('Name');
+  const [inputValue, setInputValue] = useState('');
 
   const handleKeyDown = (event: React.KeyboardEvent): void => {
     if (event.key === "Enter") handleSubmit();
@@ -112,39 +24,35 @@ export default function SearchBar({ sx = [] }: Props) {
 
   const handleSubmit = (): void => {
     const queryParams = new URLSearchParams();
-    for (const { name, value } of terms) {
-      queryParams.set(name, value.trim());
-    }
-
-    const trimmed = value?.trim();
-    if (trimmed?.length) {
-      queryParams.set("q", trimmed);
-    }
-
-    router.replace(`/modules?${queryParams}`);
+    queryParams.set(searchKind.toLowerCase(), inputValue);
+    router.push(`/modules?${queryParams}`);
   };
 
-  const renderedChips = (
-    <Stack direction="row" spacing={1}>
-      {terms.map(c => c.node)}
-    </Stack>
-  );
+  const createMenuItem = (kind: string) => (
+    <MenuItem selected={searchKind === kind} onClick={() => setSearchKind(kind)}>
+      <Typography>{kind}</Typography>
+    </MenuItem>
+  )
 
   return (
     <Box sx={[...(Array.isArray(sx) ? sx : [sx]), { width: { mobile: "100%", tablet: "auto" } }]}>
       <Input
         placeholder="Search"
-        startDecorator={renderedChips}
-        endDecorator={
-          <Tooltip title={helpText} arrow>
-            <IconButton sx={{ mr: 0 /* this gives it more margin for some reason? */ }}>
-              <QuestionMark />
-            </IconButton>
-          </Tooltip>
-        }
-        value={value ?? undefined}
-        onChange={handleChange}
+        endDecorator={(
+          <Dropdown>
+            <MenuButton>Searching: {searchKind}</MenuButton>
+            <Menu>
+              {createMenuItem('Name')}
+              {createMenuItem('Description')}
+              {createMenuItem('Summary')}
+              {createMenuItem('Owner')}
+              {createMenuItem('Tag')}
+            </Menu>
+          </Dropdown>
+        )}
         onKeyDown={handleKeyDown}
+        value={inputValue ?? undefined}
+        onChange={e => setInputValue(e.target.value)}
         sx={theme => ({
           "--Input-radius": "43px",
           "--Input-focusedInset": "var(--any, )",
